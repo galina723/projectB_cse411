1c73b399f8933c8f0f354bfdd28a2c3013639881
winterevil <long.lethanh.cit21@eiu.edu.vn>
2024-10-11 13:21:10 +0700

    updateth_user

--- demo/src/main/java/com/example/demo/controller/user/test.java
+++ demo/src/main/java/com/example/demo/controller/user/test.java
@@ -8,77 +8,77 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class test {
 
-    @GetMapping("index.html")
+    @GetMapping("index")
     public String index() {
         return "user/index";
     }
 
-    @GetMapping("404.html")
+    @GetMapping("404")
     public String error() {
         return "user/404";
     }
     
-    @GetMapping("about.html")
+    @GetMapping("about")
     public String about() {
         return "user/about";
     }
 
-    @GetMapping("blog-details.html")
+    @GetMapping("blog-details")
     public String blogdetail() {
         return "user/blog-details";
     }
 
-    @GetMapping("blog.html")
+    @GetMapping("blog")
     public String blog() {
         return "user/blog";
     }
 
-    @GetMapping("cart.html")
+    @GetMapping("cart")
     public String cart() {
         return "user/cart";
     }
 
-    @GetMapping("checkout.html")
+    @GetMapping("checkout")
     public String checkout() {
         return "user/checkout";
     }
 
-    @GetMapping("contact.html")
+    @GetMapping("contact")
     public String contact() {
         return "user/contact";
     }
 
-    @GetMapping("faq.html")
+    @GetMapping("faq")
     public String faq() {
         return "user/faq";
     }
 
-    @GetMapping("forgot-password.html")
+    @GetMapping("forgot-password")
     public String forgotpassword() {
         return "user/forgot-password";
     }
 
-    @GetMapping("login.html")
+    @GetMapping("login")
     public String login() {
         return "user/login";
     }
 
-    @GetMapping("my-account.html")
+    @GetMapping("my-account")
     public String myaccount() {
         return "user/my-account";
     }
 
-    @GetMapping("product-details.html")
+    @GetMapping("product-details")
     public String productdetail() {
         return "user/product-details";
     }
 
-    @GetMapping("shop.html")
+    @GetMapping("shop")
     public String shop() {
         return "user/shop";
     }
 
-    @GetMapping("wishlist.html")
+    @GetMapping("wishlist")
     public String wishlist() {
         return "user/wishlist";
     }
