63d5c8c12ef2207f5fd4f1c3bc51b488b795b88f
winterevil <long.lethanh.cit21@eiu.edu.vn>
2024-10-26 14:58:29 +0700

    crudproduct

--- demo/src/main/java/com/example/demo/model/productsdto.java
+++ demo/src/main/java/com/example/demo/model/productsdto.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
-import java.sql.Date;
 
+import java.sql.Date;
+import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public class productsdto {
@@ -10,64 +11,90 @@ public class productsdto {
     private String ProductCategory;
     private String ProductDescription;
     private MultipartFile ProductMainImage;
-    private MultipartFile ProductOtherImages;
+    private List<MultipartFile> ProductOtherImages;
     private double ProductPrice;
     private int ProductQuantity;
     private Date CreateTime;
+    private String ProductStatus;
+
     public int getProductId() {
         return ProductId;
     }
+
     public void setProductId(int productId) {
         ProductId = productId;
     }
+
     public String getProductName() {
         return ProductName;
     }
+
     public void setProductName(String productName) {
         ProductName = productName;
     }
+
     public String getProductCategory() {
         return ProductCategory;
     }
+
     public void setProductCategory(String productCategory) {
         ProductCategory = productCategory;
     }
+
     public String getProductDescription() {
         return ProductDescription;
     }
+
     public void setProductDescription(String productDescription) {
         ProductDescription = productDescription;
     }
+
     public MultipartFile getProductMainImage() {
         return ProductMainImage;
     }
+
     public void setProductMainImage(MultipartFile productMainImage) {
         ProductMainImage = productMainImage;
     }
-    public MultipartFile getProductOtherImages() {
-        return ProductOtherImages;
-    }
-    public void setProductOtherImages(MultipartFile productOtherImages) {
-        ProductOtherImages = productOtherImages;
-    }
+
     public double getProductPrice() {
         return ProductPrice;
     }
+
     public void setProductPrice(double productPrice) {
         ProductPrice = productPrice;
     }
+
     public int getProductQuantity() {
         return ProductQuantity;
     }
+
     public void setProductQuantity(int productQuantity) {
         ProductQuantity = productQuantity;
     }
+
     public Date getCreateTime() {
         return CreateTime;
     }
+
     public void setCreateTime(Date createTime) {
         CreateTime = createTime;
     }
 
-    
+    public String getProductStatus() {
+        return ProductStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        ProductStatus = productStatus;
+    }
+
+    public List<MultipartFile> getProductOtherImages() {
+        return ProductOtherImages;
+    }
+
+    public void setProductOtherImages(List<MultipartFile> productOtherImages) {
+        ProductOtherImages = productOtherImages;
+    }
+
 }
--- demo/src/main/java/com/example/demo/repository/productrepository.java
+++ demo/src/main/java/com/example/demo/repository/productrepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.products;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface productrepository extends CrudRepository<products, Integer> {
-    
+    @Query(value = "SELECT COALESCE(MAX(id), 0) + 1 FROM product", nativeQuery = true)
+    int findNextProductId();
 }
