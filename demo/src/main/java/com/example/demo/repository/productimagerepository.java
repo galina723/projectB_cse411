b9d87727d3b042fda546d7a36be471cc5a5a6803
hoanggyen231 <147621826+galina723@users.noreply.github.com>
2024-11-05 10:55:34 +0700

    productdetail

--- demo/src/main/java/com/example/demo/repository/productimagerepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.productimages;
-
-public interface productimagerepository extends CrudRepository<productimages, Integer> {
-
-}
