d2cfcb620a9a5a0e404921cbe3ed4ed8fff95659
hoanggyen231 <147621826+galina723@users.noreply.github.com>
2024-11-21 10:43:46 +0700

    loginadmin

--- demo/src/main/java/com/example/demo/repository/adminrepository.java
+++ demo/src/main/java/com/example/demo/repository/adminrepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.model.admin;
 
 public interface adminrepository extends CrudRepository<admin, Integer> {
+
+    
+
 }
